5e797f96b8daeaf31cd2a3c69acf392c5aa3a435
alissonbovenzo <alisson.bovenzo@live.com>
2012-11-06 09:35:36 -0200

    #arquivo criado pronto para envio de e-mail's

---
+++ site_CloudCPP/contact.php
@@ -0,0 +1,38 @@
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<?php
+
+$email 			= 	$_POST["email"]; 
+
+$headers = "MIME-Version: 1.0\n";
+$headers .="From:". $email ." \n";
+$headers .= "Content-type: text/html; charset=iso-8859-1\n"; 
+$headers .= "Contato via website de : \n";
+$nome 			= 	$_POST["name"]; 
+$telefone 		= 	$_POST["telefone"]; 
+$assunto		=	$_POST["subject"];
+$obs		= 	$_POST["message"]; 
+#$obs 		= 	preg_replace("/(\\r)?\\n/i", "<br/>", $message);
+
+$enviou = mail("alisson.bovenzo@cloudcpp.com.br",$assunto, 
+" 
+<br/>Nome: $nome
+<br/>E-mail: $email
+<br/>Telefone: $telefone
+<br/>
+<br/>Mensagem: $obs	
+
+", $headers);
+
+if ($enviou){
+echo " <SCRIPT LANGUAGE='JavaScript' TYPE='text/javascript'>alert 
+('$nome, sua mensagem foi enviada com sucesso. ')</SCRIPT>  ";
+echo "<meta HTTP-EQUIV='Refresh' CONTENT='0;URL=http://www.cloudcpp.com.br'>";
+
+}
+
+else {
+echo " <SCRIPT LANGUAGE='JavaScript' TYPE='text/javascript'>alert 
+('$nome, não foi possivel enviar sua mensagem, tente novamente')</SCRIPT>  ";
+echo "<meta HTTP-EQUIV='Refresh' CONTENT='0;URL=http://www.cloudcpp.com.br'>";
+}
+?>
